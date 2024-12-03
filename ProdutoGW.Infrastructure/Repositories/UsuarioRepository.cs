50e530b1bf2f2356a44c0a806025f3754e8a7c7b
Guilherme L. Silva <guilherme.spw@hotmail.com>
2024-12-03 20:02:29 -0300

    Inclusão dockerfile e alteração de interfaces

--- ProdutoGW.Infrastructure/Repositories/UsuarioRepository.cs
+++ ProdutoGW.Infrastructure/Repositories/UsuarioRepository.cs
@@ -1,50 +1,48 @@
-﻿namespace ProdutoGW.Infrastructure.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutoGW.Domain.Entities;
+using ProdutoGW.Infrastructure.Data;
+using ProdutoGW.Infrastructure.Interfaces;
+
+namespace ProdutoGW.Infrastructure.Repositories
 {
-    using global::ProdutoGW.Application.Interfaces;
-    using global::ProdutoGW.Domain.Entities;
-    using global::ProdutoGW.Infrastructure.Data;
-    using Microsoft.EntityFrameworkCore;
-
-    namespace ProdutoGW.Infrastructure.Repositories
+    public class UsuarioRepository : IUsuarioRepository
     {
-        public class UsuarioRepository : IUsuarioRepository
+        private readonly ProdutoContext _context;
+
+        public UsuarioRepository(ProdutoContext context)
         {
-            private readonly ProdutoContext _context;
-
-            public UsuarioRepository(ProdutoContext context)
-            {
-                _context = context;
-            }
-
-            public async Task<IEnumerable<Usuario>> GetAllAsync()
-            {
-                return await _context.Usuarios.ToListAsync();
-            }
-
-            public async Task<Usuario?> GetByGuidAsync(Guid guid)
-            {
-                return await _context.Usuarios.FirstOrDefaultAsync(u => u.Guid == guid);
-            }
-
-            public async Task<Usuario> CreateAsync(Usuario usuario)
-            {
-                await _context.Usuarios.AddAsync(usuario);
-                await _context.SaveChangesAsync();
-                return usuario;
-            }
-
-
-            public async Task DeleteAsync(Usuario usuario)
-            {
-                _context.Usuarios.Remove(usuario);
-                await _context.SaveChangesAsync();
-            }
-
-            public async Task<Usuario?> GetByEmailAsync(string email)
-            {
-                return await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
-            }
-
+            _context = context;
         }
+
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        public async Task<Usuario?> GetByGuidAsync(Guid guid)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.Guid == guid);
+        }
+
+        public async Task<Usuario> CreateAsync(Usuario usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+
+
+        public async Task DeleteAsync(Usuario usuario)
+        {
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Usuario?> GetByEmailAsync(string email)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
     }
 }
+
